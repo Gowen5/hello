2290157cdcb3313a5875374e40dcdd5e8064854c
Gavin Owen <gowen5@huskers.unl.edu>
2020-08-20 00:10:51 +0000

    changed message to my name

--- hello.c
+++ hello.c
@@ -12,8 +12,8 @@
  
  int main(int argc, char **argv) {
      
-     printf("Hello, Gavin Owen\n");
-     
+     printf("Gavin Owen\n");
+     printf("Computer Science\n");
      return 0;
      
  }
